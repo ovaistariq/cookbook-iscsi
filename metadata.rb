2e902ed87438e4ebe16c40115bc6b3e738671cbc
Eric G. Wolfe <wolfe21@marshall.edu>
2011-10-04 10:20:52 -0400

    Fix metadata constraints in iscsi/multipath Fix node attribute in oracle recipe

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,17 @@
 maintainer       "Eric G. Wolfe"
 maintainer_email "wolfe21@marshall.edu"
 license          "Apache 2.0"
-description      "Installs/Configures iscsi"
+description      "Installs configures open-iscsi for storage arrays"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 depends          "multipath"
 depends          "el-sysctl"
 recommends       "dbench"
 conflicts        "sysctl"
+
+%w{ redhat centos scientific }.each do |os|
+  supports os, ">= 5.0"
+end
 
 # Attributes for this recipe.
 attribute "iscsi/session/timeo/replacement_timeout",
