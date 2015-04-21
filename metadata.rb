ea5cef2011cd8d8ad779a63740649821ed1fe59d
Ovais Tariq <ovaistariq@gmail.com>
2015-04-21 14:42:03 -0700

    Version bump the cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'wolfe21@marshall.edu'
 license 'Apache 2.0'
 description 'Installs configures open-iscsi for storage arrays'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+version '1.1.1'
 depends 'multipath'
 depends 'sysctl'
 
